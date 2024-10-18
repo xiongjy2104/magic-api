b557c9c3d310c09abccfbce2147592b116d7fa8f
xiongjy2104 <xiongjy2104@gmail.com>
2024-10-18 18:46:21 +0800

    minifix

--- magic-api-plugins/magic-api-plugin-component/src/console/dist/magic-component.1.0.0.iife.js
+++ magic-api-plugins/magic-api-plugin-component/src/console/dist/magic-component.1.0.0.iife.js
@@ -105,13 +105,13 @@ var MagicComponent = function(vue) {
   if (typeof window !== "undefined") {
     let loadSvg = function() {
       var body = document.body;
-      var svgDom = document.getElementById("__svg__icons__dom__1728644778548__");
+      var svgDom = document.getElementById("__svg__icons__dom__1729246489330__");
       if (!svgDom) {
         svgDom = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svgDom.style.position = "absolute";
         svgDom.style.width = "0";
         svgDom.style.height = "0";
-        svgDom.id = "__svg__icons__dom__1728644778548__";
+        svgDom.id = "__svg__icons__dom__1729246489330__";
         svgDom.setAttribute("xmlns", "http://www.w3.org/2000/svg");
         svgDom.setAttribute("xmlns:link", "http://www.w3.org/1999/xlink");
       }
--- magic-api-plugins/magic-api-plugin-task/src/console/dist/magic-task.1.0.0.iife.js
+++ magic-api-plugins/magic-api-plugin-task/src/console/dist/magic-task.1.0.0.iife.js
@@ -135,13 +135,13 @@ var MagicTask = function(vue) {
   if (typeof window !== "undefined") {
     let loadSvg = function() {
       var body = document.body;
-      var svgDom = document.getElementById("__svg__icons__dom__1728644770780__");
+      var svgDom = document.getElementById("__svg__icons__dom__1729246479201__");
       if (!svgDom) {
         svgDom = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svgDom.style.position = "absolute";
         svgDom.style.width = "0";
         svgDom.style.height = "0";
-        svgDom.id = "__svg__icons__dom__1728644770780__";
+        svgDom.id = "__svg__icons__dom__1729246479201__";
         svgDom.setAttribute("xmlns", "http://www.w3.org/2000/svg");
         svgDom.setAttribute("xmlns:link", "http://www.w3.org/1999/xlink");
       }
